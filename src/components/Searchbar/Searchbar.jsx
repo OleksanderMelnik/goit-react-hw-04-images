fdfacc8ed4da82e2cacb13e5ee14445347947083
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-11-19 11:26:29 +0200

    searchbar images

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,49 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { SearchbarHeader, Form, Button, Input } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    searchQuery: ``,
+export const Searchbar = ({onSubmit}) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+const  handleQueryChange = e => {
+  setSearchQuery(e.target.value);
   };
 
-  handleQueryChange = ({ currentTarget: { value } }) => {
-    this.setState({ searchQuery: value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
-    const searchQuery = this.state.searchQuery.trim();
+const handleSubmit = e => {
     e.preventDefault();
+    onSubmit(searchQuery);
+    reset();
     
-    if (searchQuery.trim() === '') {
-      toast.info('Please, enter search word!');
-      return;
-    }
-    this.props.onSubmit(searchQuery);
-    this.setState({ searchQuery: '' });
   };
 
-  render() {
-    const { searchQuery } = this.state;
+const reset = () => {
+  setSearchQuery('');
+}
     return (
-      <SearchbarHeader className="searchbar">
-        <Form className="form" onSubmit={this.handleSubmit}>
+      <SearchbarHeader>
+        <Form onSubmit={handleSubmit}>
           <Input
-            className="input"
             type="text"
             autocomplete="off"
             autoFocus
             placeholder="Search images and photos"
             name="searchQuery"
             value={searchQuery}
-            onChange={this.handleQueryChange}
+            onChange={handleQueryChange}
           />
-
-          <Button type="submit" className="button">
-            <span className="button-label">Search</span>
+          <Button type="submit">
+            <span>Search</span>
           </Button>
         </Form>
       </SearchbarHeader>
     );
   }
-}
+
